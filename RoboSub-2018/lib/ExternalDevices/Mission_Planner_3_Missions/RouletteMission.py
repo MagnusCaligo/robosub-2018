99b94ec69bf4cb4b10285ba41fd92740c1c5a6c0
Christian Gould <christian.d.gould@gmail.com>
2018-07-06 03:49:44 -0700

    Standalone RouletteMission: Ready for Testing on Front Camera

--- RoboSub-2018/lib/ExternalDevices/Mission_Planner_3_Missions/RouletteMission.py
+++ RoboSub-2018/lib/ExternalDevices/Mission_Planner_3_Missions/RouletteMission.py
@@ -6,7 +6,7 @@ import time
 
 class RouletteMission(AbstractMission):
 
-    defaultParameters = AbstractMission.defaultParameters + "dice# = 0\ngetDistanceAway = 2\n"
+    defaultParameters = AbstractMission.defaultParameters + "Bottom_Camera_Exists_In_Code = False\n"
 
     def __init__(self, parameters):
         AbstractMission.__init__(self, parameters)
@@ -14,190 +14,76 @@ class RouletteMission(AbstractMission):
 #----Things_That_Need_Initialization_----#
     def initalizeOnMissionStart(self):
 	AbstractMission.initalizeOnMissionStart(self)
-        
-	self.atWaypoint = False
-        self.detectionData = None
-	self.lastKnownDepth = self.finalWaypoint[2]
 
-        self.foundObstacles = False
-        self.diceClassNumber = int(self.parameters["dice#"])
+#       ********Roulette Detections********           
+	self.FrontCam_RouletteFound = False
+	self.BottomCam_RouletteFound= False
 
-        self.sentMessage1 = False
-        self.sentMessage2 = False
-        self.sentMessage3 = False
-	self.sentMessage4 = False
-	self.sentMessage5 = False
-	
-	self.hitBuoy = False
-	self.movingForward = True
+	#TODO Change this number to actual value
+	self.Roulette_ClassNumber = None
 
-	self.atBuoyTimer = None
-	self.lookingForObstaclesTimer = None
-	self.hitBuoyTimer = None
+	#Location of Roulette board
+	self.Roulette_Waypoint
 
-	self.depthAtRelativeMove = None
-	self.lookingMaxTime = 10
-	self.hitBuoyMaxTime = 5
 
-	self.lookingAngle = 0
-	self.lookingDifference = 20
-
-        self.src_pts = [(0,0,0),(.58,0,0),(.58,.58,0),(0,.58,0)]
-
+	#--Timing Stuff--#
+        self.rotateTimer = None
+        self.rotateWaitTime = 5
 
 #-------------------------END-------------------------#
 
-    def IS_FOUND(self):
-	if self.detectionData == None:
-		return False
-        for i,v in enumerate(self.detectionData):
-            if v[0] == int(self.parameters["dice#"]):
-                return True
-        return False
+    def Search_RouletteBoard(self):
+	#Spinning until Found
+	if self.rotateTimer == None:
+		self.rotateTimer = time.time()
+	if time.time() - self.rotateTimer >= self.rotateWaitTime:
+                self.rotateTimer = None
+                self.calculatedWaypoint = None
+        if self.calculatedWaypoint == None:
+                posedata, n,e,u, pitch, yaw, roll = self.movementController.relativeMoveXYZ(self.orientation + self.position,0,self.position[2] - self.finalWaypoint[2],0, 45, 0,0);
+                self.calculatedWaypoint = [n,e,u,yaw,pitch,roll]
+        self.moveToWaypoint(self.calculatedWaypoint);
 
-    def sortThroughDetections(self):
-        detections = []
-        if self.detectionData != None:
-	    print "Detection Data is", self.detectionData
-            for det in self.detectionData:
-                if det[0] == int(self.parameters["dice#"]):
-                    detections.append(det)
-            return detections
-        else:
-            return None
+#-----------------------END------------------------#
 
-    def Camera_1(self, Some_Bool):
-	#TODO Camera utility
-	return FOUND_DATA
+
+    def Move_Above_Board(self):
+	self.writeDebugMessage("Moving Forward...")
+	p, n, e, u, p, y, r = self.movementController.relativeMoveXYZ(self.orientation + self.position, 0, 0, -1, 0, 0, 0)
+	self.Roulette_Waypoint = [n,e,u,y,p,r]
+	self.moveToWaypoint(self.Roulette_Waypoint)
+
+
+#TODO    def Orient_Above_Board(self):
+	
+#-----------------------END------------------------#
+    def UPDATE_VIZUALS(self):
+#	FRONT CAMERA VIZUALS
+	self.FrontCam_RouletteFound = False
+	for det in self.detectionData:
+		if det[0] == self.Roulette_ClassNumber:
+			self.FrontCam_RouletteFound = True
+
+#TODO	BOTTOM CAMERA VIZUALS
+
 
 #-----The UPDATE: Essentially the main-------#
 
     def update(self):
 
-	'''
+
+	UPDATE_VIZUALS();
+
 	if( self.FrontCam_RouletteFound or self.BottomCam_RouletteFound ):
 	    if( self.BottomCam_RouletteFound ):
 		Orient_Above_Board();
 	    else:
+		self.writeDebugMessage("Will Spin Above Target...")
 		Move_Above_Board();
 
 	else:
 	    Search_RouletteBoard()
-	'''
 
-        #Approach General Waypoint
-        if self.atWaypoint == False:
-            atWaypoint = self.moveToWaypoint(self.finalWaypoint)
-            if atWaypoint:
-                if self.sentMessage1 == False:
-                    self.writeDebugMessage("At Waypoint")
-                    self.sentMessage1 = True
-                    self.atWaypoint = True
-            else:
-                self.atWaypoint = False
-                self.sentMessage1 = False
-                self.atWaypointStartTime = None
-                print "Not there"
-                return -1
-        
-        #Look For obstacles
-	if self.hitBuoy == False:
-		if self.atWaypoint == True:
-		    if not self.IS_FOUND():
-			print "looking for buoy"
-			self.sentMessage3 = False
-			if self.sentMessage2 == False:
-			    self.writeDebugMessage("Couldn't Find Obstacles")
-			    self.sentMessage2 = True
-			if self.lookingForObstaclesTimer == None:
-				self.lookingForObstaclesTimer = time.time()
-				self.lookingAngle += self.lookingDifference
-				print "Incrementing Angle"
-			if time.time() - self.lookingForObstaclesTimer >= self.lookingMaxTime:
-				self.lookingForObstaclesTimer = None
-			waypoint = self.position + self.orientation
-			waypoint[3] += self.lookingAngle
-			waypoint[2] = self.lastKnownDepth
-			self.moveToWaypoint(waypoint)
-		    else:
-			self.sentMessage2 = False
-			if self.sentMessage3 == False:
-			    self.writeDebugMessage("Found Obstacles!")
-			    self.sentMessage3 = True
-
-			self.lastKnownDepth = self.position[2]
-			detections = self.sortThroughDetections()
-			detection = detections[0] #We only need one detection so we assume that its the first one
-
-
-			#These are the image points of the buoy in the image, this will be with the source points to get buoy depth
-			img_pts = [(detection[1], detection[2]), (detection[1] + detection[3], detection[2]), (detection[1] + detection[3], + detection[2] + detection[4]), (detection[1], detection[2] + detection[4])]
-
-			#Fake camera matrix
-			cameraMatrix = [[808,     0, 404],
-						[   0, 608, 304],
-						[   0,     0,  1.0],]
-
-
-			#Solve PNP returns the rotation vector and translation vector of the object
-			rvec, tvec = cv2.solvePnP(np.array(self.src_pts).astype('float32'), np.array(img_pts).astype('float32'),np.array(cameraMatrix).astype('float32'), None)[-2:]
-			#print "Tvec was", tvec
-			tvec[0][0]-=.25 #Camera isn't centered with Percy, so move it over a bit
-			
-			center = detection[1] + (detection[3]/2)
-			
-			rotationDifference = math.degrees(math.atan2(tvec[0], tvec[2]))
-	    
-			if abs(tvec[2]) - int(self.parameters["getDistanceAway"]) <= self.generalDistanceError and abs(rotationDifference) <=self.generalRotationError-1:
-				self.atBuoyLocation = True
-				if self.sentMessage4 == False:
-					self.writeDebugMessage("Found Buoy")
-					self.sentMessage4 = True
-					self.sentMessage5 =  False
-				if self.atBuoyTimer == None:
-					self.atBuoyTimer = time.time()
-				if time.time() - self.atBuoyTimer > float(self.parameters["waitTime"]):
-					self.hitBuoy = True
-			else:
-				if self.sentMessage5 == False:
-					self.writeDebugMessage("Not at Buoy Location")
-					self.sentMessage5 = True
-					self.sentMessage4 = False
-
-	    
-			#print "Up and Down are", tvec[1][0] +.7, "rotationDifference:", rotationDifference, "Distance Away:", -(tvec[2][0]- int(self.parameters["getDistanceAway"]))
-			#print "Distance: ", self.parameters["getDistanceAway"]
-			#poseData, north, east, up, yaw, pitch, roll =	self.movementController.relativeMoveXYZ(self.orientation+self.position, tvec[0][0], tvec[1][0], ,0,0,0)
-			print "Rotation difference was", rotationDifference
-			poseData, north, east, up, pitch, yaw, roll =	self.movementController.relativeMoveXYZ(self.orientation+self.position, tvec[0][0], tvec[1][0] + 1, -(tvec[2][0]- int(self.parameters["getDistanceAway"])),rotationDifference,0,0)
-
-			print "Errors were", north, east, up, yaw
-			print "Location is", self.position[0], self.position[1], self.position[2]
-
-			#self.movementController.relativeMoveXYZ(self.orientation+self.position, 0, tvec[1][0], 1,0,0,0)
-			#print "Errors were", north, east, up, yaw, pitch, roll
-			self.moveToWaypoint([north, east, up, yaw, pitch, roll])
-			#self.movementController.advancedMove(poseData, north, east, up, 0, yaw, 0)
-
-	elif self.hitBuoy == True:
-		print "Trying to hit buoy"
-		if self.hitBuoyTimer == None:
-			self.hitBuoyTimer = time.time()
-			self.writeDebugMessage("Moving Forward...")
-			p, n, e, u, p, y, r = self.movementController.relativeMoveXYZ(self.orientation + self.position, 0, 0, -int(self.parameters["getDistanceAway"])-1, 0, 0, 0)
-			self.diceWaypoint = [n,e,u,y,p,r]
-		if time.time() - self.hitBuoyTimer >= self.hitBuoyMaxTime:
-			if self.movingForward == False:
-				return 1 #Finished the mission
-			self.movingForward = False
-			self.writeDebugMessage("Moving Backward...")
-			p, n, e, u, p, y, r = self.movementController.relativeMoveXYZ(self.orientation + self.position, 0.5, self.position[2] - self.depthAtRelativeMove, 3*int(self.parameters["getDistanceAway"]), 0, 0, 0)
-			self.diceWaypoint = [n,e,u,y,p,r]
-			self.hitBuoyTimer = time.time()
-		if self.depthAtRelativeMove == None:
-			self.depthAtRelativeMove = self.position[2]
-		#self.movementController.advancedMove(poseData, north, east, up, 0, yaw, 0)
-		self.moveToWaypoint(self.diceWaypoint)
 
 
 
